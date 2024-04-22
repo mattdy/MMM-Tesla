9efc3b8051a2f987da393435beb37bb24a0b00ea
Matt Dyson <matt@thedysons.net>
2024-04-22 11:25:00 +0000

    Migrate to new config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        files: ["**/*.js"],
+        rules: {
+            "semi": "error",
+            "no-unused-vars": "error"
+        },
+        languageOptions: {
+            globals: {
+                buildUrl: "readonly",
+                config: "readonly",
+                DataItemProvider: "readonly",
+                empty: "readonly",
+                Log: "readonly",
+                MM: "readonly",
+                Module: "readonly",
+                moment: "readonly"
+            }
+        }
+    }
+];
