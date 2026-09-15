c73b4b139e278cee3fa104455351efa1c067e9ab
Matt Dyson <matt@thedysons.net>
2026-09-15 11:57:41 +0100

    Fix broken lint tooling. Perform a pass with prettier on root files that had previously been missed.

--- DataItemProvider.js
+++ DataItemProvider.js
@@ -2,7 +2,7 @@
  * This is the base class for any extension that provides a data item
  */
 
-var DataItemProvider = Class.extend({
+const DataItemProvider = Class.extend({
   icon: null,
   field: null,
   value: null,
@@ -14,8 +14,8 @@ var DataItemProvider = Class.extend({
 
   // Called when this provider is loaded, provides a copy of the configuration of the module
   init: function (context) {
-    this.config = context.config;
-    this.context = context;
+    this.config = context.config
+    this.context = context
   },
 
   // Called when the main module is started
@@ -25,13 +25,13 @@ var DataItemProvider = Class.extend({
   // We should use this to update the icon, field and value variables
   // This should be overridden in any sub-classes
   onDataUpdate: function (data) {}
-});
+})
 
 // Collection of all DataItemProviders that are registered with the module
-DataItemProvider.providers = [];
+DataItemProvider.providers = []
 
 // Register a new DataItemProvider with the module
 DataItemProvider.register = function (identifier, details) {
   DataItemProvider.providers[identifier.toLowerCase()] =
-    DataItemProvider.extend(details);
-};
+    DataItemProvider.extend(details)
+}
